80104c47c74da90b48d4c777d618a9e02eeb2d9f
Satfer <522302019@qq.com>
2019-09-20 16:07:20 +0800

    get_road_point.py

---
+++ get_road_point.py
@@ -0,0 +1,64 @@
+def get_road_point(point_source, point_destination, gap = 5):
+    x1 = point_source[0]
+    y1 = point_source[1]
+    x2 = point_destination[0]
+    y2 = point_destination[1]
+    if (x2 == x1):
+        direction = 1
+        if (y2 < y1):
+            direction = -1
+        return [ [x1, y] for y in range(y1 + gap*direction, y2, gap*direction)]
+    if (y2 == y1):
+        direction = 1
+        if (x2 < x1):
+            direction = -1
+        return [ [x, y1] for x in range(x1 + gap*direction, x2, gap*direction)]
+
+    a = (y2 - y1) / (x2 - x1)
+    b = y2 - a * x2
+    f = lambda x: a*x+b
+    fv = lambda y: y/a-b
+    between = lambda r,s,e: r >= min(s, e) and r <= max(s, e)
+    res = []
+    direction = [1,1]
+    if (x2 < x1):
+        direction[0] = -1
+    if (y2 < y1):
+        direction[1] = -1
+    x = x1
+    y = y1
+    res.append([x,y])
+    while (1):
+        if ( between( f( x + gap*direction[0] ), y, y + gap*direction[1] ) ):
+            x += gap*direction[0]
+        elif ( between( fv( y + gap*direction[1] ), x, x + gap*direction[0] ) ):
+            y += gap*direction[1]
+        else:
+            print("不应该呀")
+            exit()
+
+        #if( not between(x, x1, x2) and between(y, y1, y2) ):
+        if (x == x2 or y == y2 or not between(x, x1, x2) and not between(y, y1, y2)):
+            break
+        res.append([x,y])
+        #print([x,y], "\n")
+
+    return res
+
+if __name__ == "__main__":
+    print(get_road_point([0,0], [0,15]))
+
+    print(get_road_point([0,0], [15,0]))
+
+    print(get_road_point([0,0], [10,-15]))
+    
+    print(get_road_point([0,0], [15,10]))
+
+    
+    print(get_road_point([0,0], [-15,-10]))
+
+    
+    print(get_road_point([0,0], [15,11]))
+
+    
+    print(get_road_point([0,0], [15,15]))
