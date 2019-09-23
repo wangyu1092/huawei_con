72616cf8db48ff3ef2ad31c9aad5a51b25291370
wbxing <wenbiaoxing@gmail.com>
2019-09-23 11:07:12 +0800

    XGBOOST

--- XGBoost_test.py
+++ XGBoost_test.py
@@ -1,60 +1,60 @@
-import numpy as np
-import pandas as pd
-import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from xgboost import XGBRegressor
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.svm import SVR
-from sklearn.ensemble import RandomForestRegressor
+import time
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
 import math
+from get_road_point import get_road_point, get_kv_point2prop
 
-dataload = "D:\\FDU\\19shumo"
 
-# 这里读入的是处理之后的数据
-train = pd.read_csv(dataload + '/train .csv')
-# test = pd.read_csv("dataset/test2.csv")
 
-# Label 提取
-# 删除训练数据的 Label
-train_X = train.drop('RSRP', axis=1)
-# Label 单独成列
-train_Y = train.loc[:, 'RSRP']
+def train_xgboost(dataload):
 
-# 参数调优
-X_train, X_test, y_train, y_true = train_test_split(train_X, train_Y, test_size=0.2)
-true_list = y_true.tolist()
+    df_data = pd.read_csv("D:/FDU/19shumo/train_set2/train.csv")
+    train_X = df_data.drop(columns=["RSRP"])
+    train_Y = df_data['RSRP']
+    # 参数调优
+    X_train, X_test, y_train, y_true = train_test_split(train_X, train_Y, test_size=0.98)
+    # list1 = y_true.tolist()
+    true_list = y_true.tolist()
+    # true_list = []
+    # for i in range(len(list1)):
+    #     true_list.append(list1[i][0])
 
-params = [1800]
-for param in params:
-    print(param)
+    params = [5]
+    for param in params:
+        model = XGBRegressor(learning_rate=0.01,
+                             n_estimators=1000,
+                             max_depth=8,
+                             gamma=0.05,
+                             min_child_weight=1,
+                             seed=0,
+                             subsample=0.8,
+                             colsample_bytree=0.8,
+                             reg_alpha=0,
+                             reg_lambda=1)
+        model.fit(X_train, y_train)
 
-    model = XGBRegressor(learning_rate=0.01,
-                         n_estimators=param,
-                         max_depth=12,
-                         gamma=0.05,
-                         min_child_weight=1,
-                         seed=0,
-                         subsample=0.8,
-                         colsample_bytree=0.8,
-                         reg_alpha=0,
-                         reg_lambda=1)
-    model.fit(X_train, y_train)
-    y_pre = model.predict(X_test)
-    y_pre_list = y_pre.tolist()
-
-    # 计算均方误差
-    s = 0
-    for i in range(len(true_list)):
-        s += ((y_pre_list[i] - true_list[i])**2)
-    mse = s / len(true_list)
-    rmse = math.sqrt(mse)
-    print(rmse)
+        model.get_booster().save_model('D:/model_18_100.py')
+        # y_pre = model.predict(X_test)
+        # y_pre_list = y_pre.tolist()
+        # # 计算均方误差
+        # s = 0
+        # for i in range(len(true_list)):
+        #     # print("true = ", true_list[i])
+        #     # print("pre = ", y_pre_list[i])
+        #     s += ((y_pre_list[i] - true_list[i])**2)
+        # mse = s / len(true_list)
+        # rmse = math.sqrt(mse)
+        # print(param)
+        # print("rsme = ", rmse)
 
 """
 # 预测代码
 model = XGBRegressor(learning_rate=0.01,
-                     n_estimators=2800,
-                     max_depth=12,
+                     n_estimators=700,
+                     max_depth=8,
                      gamma=0.05,
                      min_child_weight=1,
                      seed=0,
@@ -72,3 +72,18 @@ ans = pd.DataFrame(Y, columns=['price'])
 test_id = pd.read_csv("dataset/test2.csv")
 pd.concat([test_id['id'], ans], axis=1).to_csv('ans/xgb_ans.csv', index=False)
 """
+
+
+if __name__ == "__main__":
+    dataload = "D:/FDU/19shumo/train_set2"
+    start_xgb = time.time()
+    train_xgboost(dataload)
+    end_xgb = time.time()
+    print("xgb time = ", end_xgb - start_xgb)
+
+
+    # tar = xgb.Booster(model_file='xgb.model')
+    # x_test = xgb.DMatrix(x_test)
+    # pre = tar.predict(x_test)
+    # act = y_test
+    # print(mean_squared_error(act, pre))
